9eb3af10fac0b7ed60619513cd9c0f8b49e41b48
Muhriddin23 <76955233+Muhriddin23@users.noreply.github.com>
2021-05-30 16:36:33 +0500

    kontent uzb

--- js/script.js
+++ js/script.js
@@ -144,6 +144,27 @@ var arrLang = {
         'adres': '9 Ziyolilar ko\'chasi, Тошкент',
         'hamkorlarimiz': 'Bizning hamkorlaimiz.',
         'huquqlar': '2021 Barcha huquqlar himoyalangan',
+        'qulaylik': 'Qulaylik',
+        'onlayn': 'Onlayn',
+        'kontakt': 'Kontakt',
+        'baholash': 'Baholash',
+        'qulaylik1': 'Muammoning bosqichma-bosqich tavsifi',
+        'qulaylikikki': 'Veb-saytda ilovadan foydalanish bo\'yicha ko\'rsatmalarni o\'qing.',
+        'qulaylikuch': 'Citizens.uz ilovasida shaxsiy kabinet yaratish uchun ma\'lumotlaringizni kiriting telefon raqamingizni, ismi va familiyasini ko\'rsating. Ko\'p etepdan o\'tishning hojati yo\'q ro\'yxatdan o\'tish bosqichlari va o\'z navbatingizni kutib vaqtni behuda sarflamaysiz. Muamoni suratga oling va taqdim etilgan ro\'yxatdan uning turini tanlang va qisqacha tavsiflang.',
+        'qulaylik4': 'Asosiy menyuda siz yuborgan xabarlarning holati ko\'rsatiladi.Siz yuborgan barcha xabarlaringizni nafaqat muammolar ro\'yxatida ko\'rishingiz mumkin, balki o\'z shahringizni xaritasida korishingiz mumkin.',
+        'onlayn1': 'Doim aloqada',
+        'onlayn2': 'Sizning xabarlaringiz onlayn tarzda yuboriladi.',
+        'onlayn3': 'Endi to\'g\'ridan-to\'g\'ri hokimiyat yoki boshqa mas\'ul shaxslar bilan bog\'lanishingiz shart emas . Sizning barcha xabarlaringiz Citizens.uz ilovasi bilan siz uchun qulay bo\'lgan har qanday vaqtda Onlayn formatda qabul qilinadi.',
+        'onlayn4': 'Jarayonlarni mas\'ul tashkilotlarga yozish endi o\'tmishda qoldi, barcha takliflar va boshqa ma\'lumotlar elektron shaklda va sizning shaxsiy kabinetingizda mavjud.',
+        'kontakt1': 'Axborot almashish',
+        'kontakt2': 'Mas\'ul muassasalar uchun professional aloqa tizimi',
+        'kontakt3': 'Taqdim etilgan muammo bilan bog\'liq masalalar bo\'yicha hokimiyat va fuqarolar bilan o\'zaro hamkorlik, Citizens.uz ilovasi orqali hal qilindi.Bundan tashqari, hokimlik xodimlari zudlik bilan vakillar bilan bog\'laning mas\'ul tashkilotlar va shuningdek, axborot tizimi orqali o\'zaro aloqada bo\'lishadi Citizens.uz, ko\'rib chiqish va qaror qabul qilish maqsadida hokimiyat va mas\'ul tashkilotlar o\'rtasida foydalanish uchun maxsus ishlab chiqilgan. Xabar va uni hal qilish bo\'yicha ko\'rilgan choralarni ko\'rib chiqqandan so\'ng, fuqarolar natijalarini etkazish fotosurat ko\'rinishidagi voqealar.',
+        'baholash1': 'Qayta aloqa',
+        'baholash2': 'Endi natijani baholash uchun imkoniyat bor',
+        'baholash3': 'Ilova fuqarolar va hokimiyat o\'rtasida barqaror aloqalarni ta\'minlaydi aholini turmush sharoitini yaxshilash va yaxshilash.Ilova foydalanuvchilari  taqdim etilgan tugatgandan so\'ng, mas\'ul tashkilotlar xodimlarining ishlarini baholash imkoniyati muammoni hal qilish bo\'yicha choralar.',
+        'baholash4': 'Sizning muammoingizni hal qilishda xodimlarning qilgan ishlarini 5 ball bilan baholash mumkin  o\'lchov Foydalanuvchilarning fikr- mulohazalari sizning kuzatuvlaringizni yaxshilash uchun muhimdir shahar.',
+                               
+                                
     },
     'ru': {
         'asosiy': 'В НАЧАЛО',
@@ -176,7 +197,28 @@ var arrLang = {
         'zagruzitinfo': 'Мобильное приложение доступно на платформах AppStore и PlayMarket. Присоединяйтесь к Citizens.uz прямо сейчас.',
         'adres': 'Ул. Зиёлилар 9, Ташкент',
         'hamkorlarimiz': 'Наши партнеры.',
-        'huquqlar': '2021 Все права защищены'
+        'huquqlar': '2021 Все права защищены',
+        'qulaylik': 'Легкость',
+        'onlayn': 'Онлайн',
+        'kontakt': 'Контакт',
+        'baholash': 'Оценка',
+        'qulaylik1': 'Пошаговое описание проблемы',
+        'qulaylikikki': 'Ознакомьтесь с инструкцией использования приложения на сайте.',
+        'qulaylikuch': 'Для того чтобы создать персональный кабинет в приложении Citizens.uz, укажите свои данные в виде номера телефона, имении и фамилии.Вам ненужно проходить многочиленные этапы регистрации и терять свое время в ожидании своей очереди.Достаточно сфотографировать  проблему, выбрать ее тип из предоставленного списка, и изложить кратко ее описание.',
+        'qulaylik4': 'В главном меню отображаются состояния, отправленных вами сообщений. Вы можете просмотреть все отправленные вами сообщения не только в списке проблем, но и наглядно, на карте вашего города.',
+        'onlayn1': 'Всегда на связи',
+        'onlayn2': 'Ваши сообщения будут отправлены в онлайн режиме.',
+        'onlayn3': 'Теперь Вам ненужно напрямую связываться с хокимиятом или с другими ответственными учреждениями для сообщения о проблемах или о предложениях, способствующих облогораживанию  вашего города.Все Ваши сообщения будут приняты  в онлайн формате с приложением Citizens.uz в любое удобное Вам время.',
+        'onlayn4': 'Процессы письменного сообщения в ответственные организации теперь в прошлом, все предложения и другие данные находятся в электронном виде и доступны в Вашем персональном кабинете.',
+        'kontakt1': 'Информационный обмен',
+        'kontakt2': 'Система профессиональной связи для ответственных учреждений',
+        'kontakt3': 'Взаимодействие с хокимиятом и гражданами по вопросам, связанными с отправленной проблемой, решается посредством приложения Citizens.uz.Кроме того, сотрудники хокимията оперативно связываются с представителями ответственных организаций и взаимодействуют также посредством информационной системы Citizens.uz, разработанный специально для пользования между хокимиятом и ответственными организациями в целях рассмотрения и решения  проблем граждан.После рассмотрения сообщения и принятых мер по его решению, граждан оповещают о результатах проведенных мероприятий в виде фотографии.',
+        'baholash1': 'Обратная связь',
+        'baholash2': 'Теперь есть возможность оценить результат ',
+        'baholash3': 'Приложение обеспечивает устойчивые связи между гражданами и хокимиятом, в целях  облагораживания и улучшения условий жизни населения.Пользователям приложения предоставляется возможность оценить работу сотрудников ответственных организаций, после завершения  мероприятий по решению проблемы.',
+        'baholash4': 'Проведенную работу сотрудников в решении Вашей проблемы, можно оценить по 5-ти бальной шкале.Отзывы от пользователей важны для улучшения качества последующих мероприятий в вашем городе.',
+                                
+                                
     }
 }
 
